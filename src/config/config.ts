7349f65543d8baab30f69b2edcf1798f5e3e58b9
Mally <no-reply@mally.qzz.io>
2026-06-15 09:43:16 +0700

    feat: get config route

--- src/config/config.ts
+++ src/config/config.ts
@@ -29,4 +29,28 @@ configRoute.post("/sync", async (c) => {
     return c.text("Success", 200)
 })
 
+configRoute.get("/sync", async (c) => {
+    const authHeader = c.req.header("Authorization")
+    const token = authHeader?.replace("Bearer ", "")
+
+    if (!token) {
+        return c.text("Unauthorized", 401)
+    }
+
+    const raw = await env.KV.get(`token:${token}`)
+    if (!raw) {
+        return c.text("Unauthorized", 401)
+    }
+
+    const session: Session = JSON.parse(raw)
+
+    const savedConfig = await env.KV.get(`config:${session.userId}`)
+
+    if (!savedConfig) {
+        return c.text("Error when trying to getting your configuration", 500)
+    }
+
+    return c.text(savedConfig, 200)
+})
+
 export default configRoute;
