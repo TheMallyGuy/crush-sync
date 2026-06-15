e31f6395ce8043e50e6c471f23834063418231f8
Mally <no-reply@mally.qzz.io>
2026-06-15 11:17:06 +0700

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import authRoute from "./auth/auth"
 import configRoute from './config/config'
 
 const app = new Hono()
+
+app.use("/auth/*", cors())
+app.use("/config/*", cors())
 
 app.route("/auth", authRoute)
 app.route("/config", configRoute)
